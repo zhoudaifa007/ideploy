31cc511d698f3189098336d6dcdd2264371ecc4d
zhoudaifa007 <zhoudaifa007@gmail.com>
2017-07-07 14:15:19 +0800

    test

---
+++ paramiko_cmd.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import paramiko
+
+host  = '172.20.32.157'
+user = 'root'
+password = 'Midea@123'
+
+def sftp_upload_file(server_path, local_path):
+    t = paramiko.Transport((host, 22))
+    t.connect(username=user, password=password)
+    sftp = paramiko.SFTPClient.from_transport(t)
+    sftp.put("F:/work1/cloud_dts1/cloud-dts-parent/cloud-dts-track/cloud-dts-track-provider/target/cloud-dts-track-provider-0.0.1-SNAPSHOT.jar", "/app/zdf/cloud-dts-track-provider-0.0.1-SNAPSHOT.jar")
+    t.close()
+
+
+if __name__ == '__main__':
+    sftp_upload_file("/root/bug.txt", "D:/bug.txt")
---
+++ paramiko_file.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+import paramiko
+
+host  = '172.20.32.157'
+port = 22
+user = 'root'
+password = 'Midea@123'
+src = "F:/work1/cloud_dts1/cloud-dts-parent/cloud-dts-track/cloud-dts-track-provider/target/cloud-dts-track-provider-0.0.1-SNAPSHOT.jar"
+dst = "/app/zdf/cloud-dts-track-provider-0.0.1-SNAPSHOT.jar"
+
+class SFTP:
+    def __init__(self,host,port, user, password):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+    def connect(self):
+        self.t = paramiko.Transport((self.host, self.port))
+        self.t.connect(username=user, password=password)
+        self.sftp = paramiko.SFTPClient.from_transport(self.t)
+    def upload(self,src, dst):
+        self.sftp.put(src, dst)
+    def close(self):
+        self.t.close()
+        self.sftp.close()
+
+if __name__ == '__main__':
+    mySFTP = SFTP(host,port,user,password)
+    mySFTP.connect()
+    mySFTP.upload(src,dst)
+    mySFTP.close()
